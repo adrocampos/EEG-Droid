2e188fe3af2e3647978d14f86930fece0c8bd79f
adrocampos <rrojascampos@uni-osnabrueck.de>
2019-05-12 12:41:16 +0200

    Load the CSV file as ArrayList<Float[]>

--- app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/Display.java
+++ app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/Display.java
@@ -6,15 +6,21 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.io.File;
-import java.util.ArrayList;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Display extends AppCompatActivity {
 
-
     private ArrayList<File> arrayListOfFiles;
     private String[] arrayOfNames;
+    private File fileToPlot;
+    List<float[]> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +30,67 @@ public class Display extends AppCompatActivity {
         ManageSessions manager = new ManageSessions();
         arrayListOfFiles = manager.getArrayListOfFiles();
 
+        //Create the list of names for display in dialog
         arrayOfNames = new String[arrayListOfFiles.size()];
         for(int i=0; i < arrayOfNames.length; i++) {
             arrayOfNames[i] = arrayListOfFiles.get(i).getName();
         }
 
+        //Dialog for choosing the session to plot
         AlertDialog.Builder alert = new AlertDialog.Builder(this)
                 .setTitle(R.string.select_recording)
                 .setItems(arrayOfNames, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        // The 'which' argument contains the index position
-                        // of the selected item
+
                         Log.d("test", arrayListOfFiles.get(which).getName()); // Here we access to the file
+                        fileToPlot = arrayListOfFiles.get(which);
+                        loadData(fileToPlot);
                         dialog.dismiss();
-                        //Log.d("Display", Integer.toString(which));
+                        //plot the data
+
+
                     }
                 });
         alert.show();
+
+
+
     }
 
-    private void plot(File file){
+
+
+
+
+    private void loadData (File file) {
+
+        data = new ArrayList<float[]>();
+
+        try {
+            // create csvReader object and skip first 3 Lines
+            CSVReader csvReader = new CSVReaderBuilder(new FileReader(file)).withSkipLines(3).build();
+
+            String[] values;
+            while ((values = csvReader.readNext()) != null) {
+
+                float[] intFloats = new float[values.length];
+                for (int i=0; i < values.length-1; i++) {
+                    intFloats[i] = Float.parseFloat(values[i]);
+                }
+                data.add(intFloats);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private void plot (){
 
     }
+
 
 
 
