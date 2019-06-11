ff7c27290bc1084513b31145a556534ca311ac6f
adrocampos <rrojascampos@uni-osnabrueck.de>
2019-06-11 15:56:49 +0200

    Trying to use WebView

--- app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/Learn.java
+++ app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/Learn.java
@@ -1,19 +1,20 @@
 package de.uni_osnabrueck.ikw.eegdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebView;
 
-import us.feras.mdv.MarkdownView;
 
 public class Learn extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
 
-        MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownLearn);
-        markdownView.loadMarkdownFile("https://raw.githubusercontent.com/adrocampos/EEG-Droid/master/learning/epilepsy.md");
+        webView = (WebView) findViewById(R.id.webViewLearn);
+        webView.loadUrl("https://raw.githubusercontent.com/adrocampos/EEG-Droid/master/learning/epilepsy.html");
     }
 }
