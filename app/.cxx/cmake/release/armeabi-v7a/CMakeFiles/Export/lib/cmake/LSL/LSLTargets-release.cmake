63e5f31c775e59df2468fd98147791fa5a3e404a
mvidaldp <mvidal.dp@gmail.com>
2021-09-29 21:37:48 +0200

    Update liblsl to version 1.15.2

---
+++ app/.cxx/cmake/release/armeabi-v7a/CMakeFiles/Export/lib/cmake/LSL/LSLTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "LSL::lsl" for configuration "Release"
+set_property(TARGET LSL::lsl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(LSL::lsl PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblsl.so"
+  IMPORTED_SONAME_RELEASE "liblsl.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS LSL::lsl )
+list(APPEND _IMPORT_CHECK_FILES_FOR_LSL::lsl "${_IMPORT_PREFIX}/lib/liblsl.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
